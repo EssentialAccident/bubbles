e9add78c706d694118c03f45828c8340fe86ffad
Lino Oropeza <linooropeza@gmail.com>
2019-12-08 21:37:40 -0500

    Added more than one bubble

--- sketch.js
+++ sketch.js
@@ -1,27 +1,40 @@
 // Global variables
-let bubble;
-let push;
+let bubbles;
+let pushBubblesBtn;
+let addBubbleBtn;
 
 function setup() {
     // Setup for the sketch
     createCanvas(600, 600);
-    bubble = new Bubble(
-        createVector(random(width), random(height)),
-        random(10, 50),
-        color(int(random(255)), int(random(255)), int(random(255))
-        ));
-    push = createButton('Bounce!!!');
-    push.position(20, 610);
-    push.mousePressed(applyForce);
+    bubbles = [];
+    pushBubblesBtn = createButton('Bounce!!!');
+    pushBubblesBtn.position(10, 10);
+    pushBubblesBtn.mousePressed(applyForce);
+    addBubbleBtn = createButton('Add bubble!!!');
+    addBubbleBtn.position(100, 10);
+    addBubbleBtn.mousePressed(addBubble)
 }
 
 function draw() {
     // Draws the sketch 60 times a second
     background(0);
-    bubble.update();
-    bubble.show();
+    for (let bubble of bubbles) {
+        bubble.update();
+        bubble.show();
+    }
+
 }
 
 function applyForce() {
-    bubble.applyForce(createVector(random(-5, 5), random(-5, 5)))
+    for (let bubble of bubbles) {
+        bubble.applyForce(createVector(random(-5, 5), random(-5, 5)))
+    }
+}
+
+function addBubble() {
+    bubbles.push(new Bubble(
+        createVector(random(width), random(height)),
+        random(10, 50),
+        color(int(random(255)), int(random(255)), int(random(255))
+        )));
 }
